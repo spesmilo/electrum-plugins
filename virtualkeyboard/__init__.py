77364194602939a9196208bc3f80b270e90894cc
ThomasV <thomasv@electrum.org>
2024-09-28 09:52:40 +0200

    rm zip file

--- virtualkeyboard/__init__.py
+++ virtualkeyboard/__init__.py
@@ -3,7 +3,7 @@ from electrum.i18n import _
 fullname = 'Virtual Keyboard'
 description = '%s\n%s' % (_("Add an optional virtual keyboard to the password dialog."), _("Warning: do not use this if it makes you pick a weaker password."))
 available_for = ['qt']
-download_url = 'https://raw.githubusercontent.com/spesmilo/electrum-plugins/master/virtualkeyboard-0.0.1.zip'
+download_url = 'https://github.com/spesmilo/electrum-plugins/releases/download/0.0.1/virtualkeyboard-0.0.1.zip'
 author = 'The Electrum developers'
 licence = 'MIT'
 version = '0.0.1'
