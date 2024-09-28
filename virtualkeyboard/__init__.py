cab9c7d062377b1bdd15c10d41d8c9d918b49d8d
ThomasV <thomasv@electrum.org>
2024-09-28 11:13:53 +0200

    files

--- virtualkeyboard/__init__.py
+++ virtualkeyboard/__init__.py
@@ -3,7 +3,6 @@ from electrum.i18n import _
 fullname = 'Virtual Keyboard'
 description = '%s\n%s' % (_("Add an optional virtual keyboard to the password dialog."), _("Warning: do not use this if it makes you pick a weaker password."))
 available_for = ['qt']
-download_url = 'https://github.com/spesmilo/electrum-plugin-virtualkeyboard/releases/download/0.0.1/virtualkeyboard-0.0.1.zip'
 author = 'The Electrum developers'
 licence = 'MIT'
 version = '0.0.1'
